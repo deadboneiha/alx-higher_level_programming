c4179365a6c7e4c3652b15abb8b46988f03584a0
deadboneiha <reelghostz@gmail.com>
2023-03-10 14:18:42 -0800

    late commits

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 print(str.split(',')[2].split()[0], str.split()[-2], str.split()[-1][:-1])
